3ba161fea33ab1d5583207598238ab2c4349c759
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-02-26 21:45:35 -0500

    ack works tested and return  0 and 1

---
+++ Assignment1Scripts/client.py
@@ -0,0 +1,55 @@
+import sys
+import socket
+import os
+
+
+def get_free_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(("", 0))
+        ip_addr, free_port = s.getsockname()
+        return free_port
+    # try/finally block to ensure that the socket is always closed, even if an exception is raised:
+    finally:
+        s.close()
+
+
+def main():
+    # Check command line arguments
+    if len(sys.argv) < 6:
+        print(
+            "Error: Expect 5 parameters <server_address>, <n_port>, <mode>, <req_code>,  <file_received> "
+        )
+        return
+
+    # type check parameters
+    try:
+        server_addr = str(sys.argv[1])
+        n_port = int(sys.argv[2])
+        mode = str(sys.argv[3])
+        req_code = int(sys.argv[4])
+        file_received = str(sys.argv[5])
+    except ValueError:
+        print("Error: check type of parameters")
+    # stage 1. Negotiation using UDP sockets
+    r_port = get_free_port()
+    print("r_port (range 1025-65535):", r_port)
+    # create udp socket
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # depends on the mode:
+    if mode == "A":
+        # need to send PORT <r_port> <req_code>
+        PORT_msg = "PORT" + str(r_port)+"|" + str(req_code)
+        print(PORT_msg.encode(), (server_addr, n_port))
+        udp_socket.sendto(PORT_msg.encode(), (server_addr, n_port))# default encode is UTF-8
+
+    elif mode == "P":
+        print("P")
+    # receive r_port from server
+
+    # Create a TCP/IP socket
+    # tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment1Scripts/server.py
@@ -0,0 +1,80 @@
+import sys
+import socket
+import os
+
+
+# -*- coding: utf-8 -*-
+
+def get_free_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # tcp connection
+        s.bind(('', 0))
+        ip_addr, free_port = s.getsockname()  # return (ip address, port)
+        # print(free_port)
+        return free_port
+
+
+# get_free_port()
+def main():
+    # Check command line arguments
+    if len(sys.argv) != 3:
+        print("check number of argument failed")
+        exit(-1)
+    # check req_code be int
+    try:
+        req_code = int(sys.argv[1])
+        # print("Error: req_code should be an integer.")
+    except ValueError:
+        print("Error:sys.argv[1]/<req_code> must be int")
+
+    file_to_send = sys.argv[2]
+    # configure udp socket
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Bind the socket to a specific port
+    server_address = ('', 0)
+    udp_sock.bind(server_address)
+
+    # Print the port number the server is listening on
+    print("stage1 Negotiation using udp, <n_port> is:", udp_sock.getsockname()[1])
+
+    while True:
+        # Wait for a connection
+        print('stage1 Negotiation Server is waiting for a connection...')
+        data, client_address = udp_sock.recvfrom(1024) # max 1024 bytes
+        print("Received data:", data, " from client_address", client_address)
+        data = data.decode()
+        data=data.split("|")
+        print("data",data)
+        client_req_code = int(data[1])
+        if (client_req_code == req_code):
+        # register r_port and send 1
+            ack = 1
+            print(ack)
+        else:
+        # send 0
+            ack = 0
+            print(ack)
+        # decode data
+        """
+         try:
+            if not data:
+                print("No data received")
+                break
+        """
+        #client_req_code = int.from_bytes(data, byteorder='big')
+        #print("Received request code: client_req_code:", client_req_code)
+
+        #stage2
+
+        # Create a TCP/IP socket
+        #tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Listen for incoming connections
+        #tcp_sock.listen(1)
+
+        #finally:
+            # Clean up the connection
+        #    udp_sock.close()
+        #    print("Connection closed\n")
+
+
+if __name__ == '__main__':
+    main()
